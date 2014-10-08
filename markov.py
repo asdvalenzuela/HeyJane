59397f8405d33d37e8873bd9cca3c065ae2bcc19
Hackbright Student <info@hackbrightacademy.com>
2014-10-07 18:14:45 -0700

    posting to twitter, work in progress

--- markov.py
+++ markov.py
@@ -2,6 +2,8 @@
 
 from sys import argv
 import random
+import twitter
+api = twitter.api()
 
 script, filename = argv
 
@@ -98,7 +100,7 @@ def make_text(markov_chain_dict, n):
 
 def main():
 
-    n = 3
+    n = 6
 
     input_text = open(filename)
 
@@ -106,6 +108,12 @@ def main():
 
     random_text = make_text(chain_dict,n)
     print random_text
+
+    api = twitter.api(consumer_key='rKDZ8Ufjo9vLCZyGB56TnfzQj',consumer_secret='nSdINaV99BEkSlK10Lun3OQaQhJYVWzWuxkKQvrPtGNLsLgwP9', access_token_key='2845484612-vFQKHeUKEV24Bk2uawBhddu6z6vWrvrE6RRU2R9', access_token_secret='f2wCSxnl0Np7dy0UHTFKza9l5zXEqJP4wBhLHd6eGtKMl')
+
+
+    api.PostUpdate(random_text)
+    
 
 if __name__ == "__main__":
     main()
